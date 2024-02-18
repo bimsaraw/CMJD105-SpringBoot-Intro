267f0c0bf59dd5c3df5dd9b9d06f83e29e6440a2
Bimsara Walallawita <bwalallawita@gmail.com>
2024-02-18 13:37:25 +0530

    intro to APIs

---
+++ src/main/java/com/ijse/hellospring/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ijse.hellospring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ijse.hellospring.entity.User;
+import com.ijse.hellospring.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService{
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+}
