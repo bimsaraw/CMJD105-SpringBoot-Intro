58f100ac692ba54815bb7273661d0db027056251
Bimsara Walallawita <bwalallawita@gmail.com>
2024-03-17 13:11:57 +0530

    One to Many

--- src/main/java/com/ijse/hellospring/controller/ProductController.java
+++ src/main/java/com/ijse/hellospring/controller/ProductController.java
@@ -11,7 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ijse.hellospring.dto.ProductDto;
+import com.ijse.hellospring.entity.Category;
 import com.ijse.hellospring.entity.Product;
+import com.ijse.hellospring.service.CategoryService;
 import com.ijse.hellospring.service.ProductService;
 
 @RestController
@@ -20,6 +23,9 @@ public class ProductController {
     
     @Autowired
     private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("/products")
     public List<Product> getAllProducts() {
@@ -38,7 +44,16 @@ public class ProductController {
     }
 
     @PostMapping("/products")
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+    public ResponseEntity<Product> createProduct(@RequestBody ProductDto productDto) {
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setPrice(productDto.getPrice());
+        product.setQuantity(productDto.getQuantity());
+        
+        //find category using categoryId in the ProductDTO
+        Category category = categoryService.getCategoryById(productDto.getCategoryId());
+        product.setCategory(category);
+        
         Product createdProduct = productService.createProduct(product);
 
         return ResponseEntity.status(201).body(createdProduct);
