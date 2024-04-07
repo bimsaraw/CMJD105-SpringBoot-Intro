45ccf3936274df8fb8dc02e3f74a347e96e78a47
Bimsara Walallawita <bwalallawita@gmail.com>
2024-04-07 13:14:41 +0530

    JWT Authentication

--- src/main/java/com/ijse/hellospring/repository/UserRepository.java
+++ src/main/java/com/ijse/hellospring/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.ijse.hellospring.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.ijse.hellospring.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     //Custom queries can be added here
+    Optional<User> findByUsername(String username);
 }
